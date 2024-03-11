b40a7b4a65a6e28e2f3cb850896088ffc35c75aa
James H <47411315+MilkshakesBot@users.noreply.github.com>
2024-03-11 00:57:46 +0000

    Update dotfiles.sh since it didnt fully work before

--- dotfiles.sh
+++ dotfiles.sh
@@ -9,14 +9,10 @@ cp ~/dotfiles/.bashrc ~/.bashrc
 source .bashrc
 
 #zsh
-chsh -s $(which zsh)
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 cp ~/dotfiles/.zshrc ~/.zshrc
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/chrissicool/zsh-256color ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-256color
 git clone https://github.com/gomjellie/zsh-hangul ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-hangul
 
-source .zshrc
+echo "Please run source .bashrc/.zshrc"
